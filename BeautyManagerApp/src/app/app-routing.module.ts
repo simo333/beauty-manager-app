6b1373ef7bea366b5eb2461b8b5dfa2725b3c687
simo333 <szymon.333@wp.pl>
2022-09-29 17:37:42 +0200

    First modifications

--- BeautyManagerApp/src/app/app-routing.module.ts
+++ BeautyManagerApp/src/app/app-routing.module.ts
@@ -6,14 +6,18 @@ import {RegisterComponent} from "./register/register.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {BoardUserComponent} from "./board-user/board-user.component";
 import {BoardAdminComponent} from "./board-admin/board-admin.component";
+import {AboutComponent} from "./about/about.component";
+import {TreatmentsComponent} from "./treatments/treatments.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'profile', component: ProfileComponent},
+  {path: 'zaloguj', component: LoginComponent},
+  {path: 'zarejestruj', component: RegisterComponent},
+  {path: 'profil', component: ProfileComponent},
   {path: 'user', component: BoardUserComponent},
   {path: 'admin', component: BoardAdminComponent},
+  {path: 'o-nas', component: AboutComponent},
+  {path: 'zabiegi', component: TreatmentsComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
